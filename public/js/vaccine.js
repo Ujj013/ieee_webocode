d0387625f3840e0d7775e721736b124c5dbde215
nischaykh <nischaykh@gmail.com>
2021-06-30 22:44:22 +0530

    test

--- public/js/vaccine.js
+++ public/js/vaccine.js
@@ -1,35 +1,30 @@
 const data = [
   {
     name: 'Apollo Hospitals',
-    imgURL: '../img/hospital1.jpg',
     city: 'Mumbai',
     vaccinationsAvailable: '3k',
     vaccinationGiven: '1200',
   },
   {
     name: 'Billroth Hospitals ',
-    imgURL: '../img/hospital2.jpg',
     city: 'Mumbai',
     vaccinationsAvailable: '2k',
     vaccinationGiven: '10000',
   },
   {
     name: 'Care Hospitals',
-    imgURL: '../img/hospital3.jpg',
     city: 'Bhopal',
     vaccinationsAvailable: '1.5k',
     vaccinationGiven: '25000',
   },
   {
     name: 'Command Hospital',
-    imgURL: '../img/hospital4.jpg',
     city: 'Delhi',
     vaccinationsAvailable: '2.6k',
     vaccinationGiven: '23000',
   },
   {
     name: 'LifeSpring Hospitals',
-    imgURL: '../img/hospital5.jpg',
     city: 'Banglore',
     vaccinationsAvailable: '4k',
     vaccinationGiven: '1250',
@@ -49,6 +44,7 @@ function nextProfile() {
   const currentProfile = profiles.next().value;
 
   if (currentProfile !== undefined) {
+
     document.getElementById('profileDisplay').innerHTML = `
     <ul class="list-group">
       <li class="list-group-item">Name: ${currentProfile.name}</li>
@@ -57,9 +53,6 @@ function nextProfile() {
       <li class="list-group-item">Vaccines Given: ${currentProfile.vaccinationGiven}</li>
     </ul>
     `;
-    document.getElementById(
-      'imageDisplay'
-    ).innerHTML = `<img src="${currentProfile.imgURL}">`;
   } else {
     // No More Profiles
     window.location.reload();
